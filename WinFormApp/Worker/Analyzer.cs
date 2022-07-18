5693a93d3fdb7e8680bbf406b957db9b1dfd4be5
Darko Bondjerovic <rapidtdd@gmail.com>
2022-07-19 00:11:35 +0200

    find symbol type

--- WinFormApp/Worker/Analyzer.cs
+++ WinFormApp/Worker/Analyzer.cs
@@ -5,7 +5,7 @@ using Microsoft.CodeAnalysis.FindSymbols;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text; 
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
