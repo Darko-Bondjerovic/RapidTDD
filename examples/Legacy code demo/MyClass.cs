afe61cbe4008e8043676ea7d47520aeb63cba789
Darko Bondjerovic <rapidtdd@gmail.com>
2022-09-26 23:14:31 +0200

    Legacy code demo

---
+++ examples/Legacy code demo/MyClass.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Text;
+
+public class MyClass
+{
+    private StringBuilder str = null;
+    
+    public MyClass(int count)
+    {
+        str = new StringBuilder();
+
+        for (int i = 1; i < 2 * count; i++)
+        {
+            var m = 0;
+            if (i < count)
+                m = i;
+            else
+                m = (2 * count) - i;
+
+            var s = new StringBuilder();
+            for (int k = 0; k < m; k++)
+                s.Append(m);
+
+            str.AppendLine(s.ToString());
+        }
+    }
+}
